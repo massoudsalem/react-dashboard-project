8a9914367b95f81d01ba28354c4fb774c57dd9aa
mohamed magdy <mohamed.magdy0598@gmail.com>
2023-05-16 15:13:12 +0300

    creating progress bar for each image uploading, uploading state, faild state, complete state
    Co-authored-by: Mohammad Massoud Salem <massoudsalem@users.noreply.github.com>

--- src/components/ImageDropzone/ImageDropzone.jsx
+++ src/components/ImageDropzone/ImageDropzone.jsx
@@ -1,13 +1,13 @@
-import { Close, CloudUpload, HandymanOutlined } from '@mui/icons-material';
-import { Box, Button, IconButton, Input, Typography } from '@mui/material';
+import { Check, Close, CloudUpload } from '@mui/icons-material';
+import { Box, Button, IconButton, Input, LinearProgress, Typography } from '@mui/material';
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const ImageDropzone = () => {
   const [files, setFiles] = useState([]);
-  const [imagesURL, setImageURL] = useState([]);
   const [isUploading, setIsUploading] = useState('upload');
+  const [images, setImages] = useState({});
   const imageUpload = () => {
     if (files.length === 0) return;
     setIsUploading('uploading....');
@@ -15,24 +15,31 @@ const ImageDropzone = () => {
     formData.append('upload_preset', 'ms9f59ll');
 
     files.forEach(async (file) => {
-      setImageURL([]);
       formData.append('file', file);
+      setImages((prev) => ({ ...prev,
+        [file.name]: {
+          percent: 0,
+          failed: false,
+          url: '',
+        },
+      }
+      ));
       try {
         const response = await axios.post(
           'https://api.cloudinary.com/v1_1/djqzkpum0/image/upload',
           formData,
-          //{
-          //onUploadProgress: (progressEvent) => {
-          //console.log(
-          //`Upload Progress: ${Math.round(
-          //(progressEvent.loaded / progressEvent.total) * 100,
-          //)}%`,
-          //);
-          //},
-          //},
+          {
+            onUploadProgress: (progressEvent) => {
+              const { loaded, total } = progressEvent;
+              const percent = parseInt((loaded * 100) / total, 10);
+              setImages((prev) => ({ ...prev, [file.name]: { ...prev[file.name], percent } }));
+            },
+          },
         );
-        setImageURL((prev) => [...prev, response.data.secure_url]);
+        setImages((prev) => ({ ...prev, [file.name]: { ...prev[file.name], url: response.data.secure_url } }));
       } catch (err) {
+        setImages((prev) => ({ ...prev, [file.name]: { ...prev[file.name], failed: true } }));
+        //console.log('error uploading image');
         console.log(err);
       } finally {
         setIsUploading('uploaded');
@@ -54,7 +61,7 @@ const ImageDropzone = () => {
       );
     },
   });
-
+  //console.log(images);
   useEffect(() => {
     //Make sure to revoke the data uris to avoid memory leaks, will run on unmount
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
@@ -71,19 +78,34 @@ const ImageDropzone = () => {
           Drag &#39;n&#39; drop some files here, or click to select files
         </Typography>
       </Box>
-      <Box className="thumbsContainer flex flex-wrap gap-2">
-        {isUploading !== 'uploaded' && files.map((file) => (
+      <Box className="flex flex-wrap gap-2">
+        {files.map((file) => (
           <Box
             className="inline-flex box-border mx-1 my-4 w-28 h-28 p-2 border-solid border-[1px] border-gray-400 relative"
             key={file.name}
           >
-            <IconButton
-              className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-[2px]"
-              onClick={() => setFiles(files.filter((f) => f.name !== file.name))}
-            >
-              <Close className="text-lg" />
-            </IconButton>
-            <Box className="flex min-w-0 overflow-hidden">
+            {
+              images[file.name]?.failed && (
+                <Typography className="absolute top-0 left-0 w-full h-full bg-red-500 bg-opacity-50 text-white flex items-center justify-center">
+                  Failed
+                </Typography>
+              )
+
+            }
+            {!images[file.name]?.failed && (images[file.name]?.percent !== 100 ? (
+              <IconButton
+                className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-[2px]"
+                onClick={() => setFiles(files.filter((f) => f.name !== file.name))}
+              >
+                <Close className="text-lg" />
+              </IconButton>
+            )
+              : (
+                <Box className="absolute rounded-full top-0 right-0 w-6 h-6 translate-x-1/2 -translate-y-1/2 bg-green-500 text-white">
+                  <Check className="" />
+                </Box>
+              ))}
+            <Box className="flex flex-col overflow-hidden">
               <img
                 src={file.preview}
                 className="block object-cover w-full h-full"
@@ -92,6 +114,8 @@ const ImageDropzone = () => {
                   URL.revokeObjectURL(file.preview);
                 }}
               />
+
+              <LinearProgress className="w-full" color="secondary" variant="determinate" value={images[file.name]?.percent ?? 0} />
             </Box>
           </Box>
         ))}
