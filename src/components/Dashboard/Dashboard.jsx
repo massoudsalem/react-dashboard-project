0ab7a471098b212f098538589c05b985234f2b0e
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-06-10 18:01:37 +0300

    feat: Add reports to Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,25 +1,43 @@
-import { Box, Card, Divider, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  Divider,
+  Typography,
+  CircularProgress,
+  useMediaQuery,
+} from '@mui/material';
 import React from 'react';
 import {
-  Customers,
   HotDealsTable,
   InfoBox,
-  Products,
   TopBuyingCustomersTable,
+  ComposedCharts,
+  CustomPieChart,
 } from '..';
-import useGetHotDealsQuery from '../../hooks/useGetHotDealsQuery';
-import useGetTopBuyingCustomers from '../../hooks/useGetTopBuyingCustomers';
+import {
+  useGetCompanyPersonCitesCount,
+  useGetCustomersAgeRangeCount,
+} from '../../hooks';
 
 const Dashboard = () => {
-  const { hotDeals, isLoading, isError, error } = useGetHotDealsQuery();
-  const { topBuyingCustomers } = useGetTopBuyingCustomers();
+  const { customersAgeRangeCount, isLoading: customersAgeRangeCountIsLoading } =
+    useGetCustomersAgeRangeCount();
+  const {
+    companyPersonCitesCount,
+    isLoading: companyPersonCitesCountIsLoading,
+  } = useGetCompanyPersonCitesCount();
+
+  const isMD = useMediaQuery('(min-width:960px)');
+  const width = isMD ? 'max-w-[600px]' : 'max-w-[500px]';
 
   return (
     <Box className="flex flex-col gap-5">
-      <Box className="flex items-center justify-center">
-        <Typography variant="h4">Welcome to Dashboard</Typography>
+      <Box className="flex flex-col items-start">
+        <Typography variant="h5">Good Morning, Mo!</Typography>
+        <Typography variant="subtitle1" className="text-gray-500">
+          Here&apos;s what&apos;s happening with your store today.
+        </Typography>
       </Box>
-
       <Box className="flex flex-wrap justify-around gap-5">
         <InfoBox
           title="Total Sales"
@@ -58,14 +76,56 @@ const Dashboard = () => {
           subtitle="+1.5 %"
         />
       </Box>
+      <Box className="flex flex-col items-center justify-evenly gap-4 lg:flex-row lg:items-stretch">
+        <Box component={Card} className="w-full max-w-[300px] overflow-x-auto">
+          <Typography variant="h6" align="left" className="p-4">
+            Customers Age Range
+          </Typography>
+          <Divider />
+          {customersAgeRangeCountIsLoading ? (
+            <Box className="flex items-center justify-center">
+              <CircularProgress />
+            </Box>
+          ) : (
+            <CustomPieChart data={customersAgeRangeCount} label="Customers" />
+          )}
+        </Box>
+        <Box
+          component={Card}
+          className={`w-full ${width} overflow-x-auto lg:overflow-hidden`}
+        >
+          <Typography variant="h6" align="left" className="p-4">
+            Composed Chart
+          </Typography>
+          <Divider />
+          {companyPersonCitesCountIsLoading ? (
+            <Box className="flex items-center justify-center">
+              <CircularProgress />
+            </Box>
+          ) : (
+            <ComposedCharts
+              data={companyPersonCitesCount}
+              barLabel="Company Cities"
+              barData="companyAddress" /*the key of data for object*/
+              areaLabel="Person Cities"
+              areaData="personAddress"
+            />
+          )}
+        </Box>
+      </Box>
+
       <Box className="flex flex-col gap-4 lg:flex-row">
         <Card className="flex flex-1 flex-col items-center bg-red-200">
-          <Typography variant="h5" align='center'>Hot Deals</Typography>
+          <Typography variant="h5" align="center">
+            Hot Deals
+          </Typography>
           <Divider />
           <HotDealsTable className="flex-1" />
         </Card>
         <Card className="flex flex-1 flex-col items-center bg-green-200">
-          <Typography variant="h5" align='center'>Top Buying Customers</Typography>
+          <Typography variant="h5" align="center">
+            Top Buying Customers
+          </Typography>
           <Divider />
           <TopBuyingCustomersTable className="flex-1" />
         </Card>
