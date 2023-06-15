2411df58ac8cca2e7026b7e825a1e3f842ae4753
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-06-15 23:55:03 +0300

    fix: Customers Edit and Create Forms
    Add a date field and validate the form

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,23 +1,57 @@
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Tooltip } from '@mui/material';
 import React, { useState } from 'react';
 
 const EditForm = ({ editData, handleUpdate, handleClose, handleAddUser }) => {
   const [userInfo, setUserInfo] = useState(editData);
+  const [phoneError, setPhoneError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const phonePattern = /^\+?[0-9]{1,3} ?[0-9]{3} ?[0-9]{3} ?[0-9]{4}$/;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [triedSubmit, setTriedSubmit] = useState(false);
 
   const handleChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.id]: e.target.value });
+
+    if (e.target.id === 'phone') {
+      if (!phonePattern.test(e.target.value)) {
+        setPhoneError(true);
+      } else {
+        setPhoneError(false);
+      }
+    }
+    if (e.target.id === 'email') {
+      if (!emailPattern.test(e.target.value)) {
+        setEmailError(true);
+      } else {
+        setEmailError(false);
+      }
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (userInfo.id === undefined) {
-      handleAddUser(userInfo);
-      handleClose();
+    setTriedSubmit(true);
+    if (!phonePattern.test(userInfo.phone)) {
+      setPhoneError(true);
     } else {
-      const { id } = editData;
-      const newData = { ...userInfo };
-      handleUpdate(id, newData);
-      handleClose();
+      setPhoneError(false);
+    }
+    if (!emailPattern.test(userInfo.email)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    if (!phoneError && !emailError) {
+      setUserInfo(userInfo);
+      if (userInfo.id === undefined) {
+        handleAddUser(userInfo);
+        handleClose();
+      } else {
+        const { id } = editData;
+        const newData = { ...userInfo };
+        handleUpdate(id, newData);
+        handleClose();
+      }
     }
   };
 
@@ -28,6 +62,7 @@ const EditForm = ({ editData, handleUpdate, handleClose, handleAddUser }) => {
     >
       <TextField
         label="firstName"
+        type="text"
         id="firstName"
         defaultValue={userInfo?.firstName}
         size="small"
@@ -36,24 +71,51 @@ const EditForm = ({ editData, handleUpdate, handleClose, handleAddUser }) => {
       />
       <TextField
         label="lastName"
+        type="text"
         id="lastName"
         defaultValue={userInfo?.lastName}
         size="small"
         onChange={handleChange}
         required
       />
+      <Tooltip
+        title="Please enter a valid phone number (e.g. +1 123 456 7890)"
+        open={phoneError && triedSubmit}
+        placement="bottom"
+        arrow
+      >
+        <TextField
+          label="phone"
+          id="phone"
+          defaultValue={userInfo?.phone}
+          size="small"
+          onChange={handleChange}
+          required
+          type="tel"
+          error={phoneError && triedSubmit}
+        />
+      </Tooltip>
+      <Tooltip
+        title="Please enter a valid email address"
+        open={emailError && triedSubmit}
+        placement="bottom"
+        arrow
+      >
+        <TextField
+          label="email"
+          type="email"
+          id="email"
+          defaultValue={userInfo?.email}
+          size="small"
+          onChange={handleChange}
+          required
+          error={emailError && triedSubmit}
+        />
+      </Tooltip>
       <TextField
-        label="phone"
-        id="phone"
-        defaultValue={userInfo?.phone}
-        size="small"
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="email"
-        id="email"
-        defaultValue={userInfo?.email}
+        id="birthDate"
+        type="date"
+        defaultValue={userInfo?.birthDate}
         size="small"
         onChange={handleChange}
         required
